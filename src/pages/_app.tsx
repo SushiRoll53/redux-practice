00ab661f8248362f1b74a96cbeccdb2def4ba1c8
SushiRoll53 <bryan@pif.co>
2024-04-30 09:14:57 -0600

    Set up provider on top of the app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
+import store from "@/redux/store/store";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Provider } from 'react-redux';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
+
+export default App;
