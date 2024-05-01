b7e9c32742f7261dbbfdf8db9e85679aa5bff88e
SushiRoll53 <bryan@pif.co>
2024-05-01 09:30:02 -0600

    Add style module to counter

--- src/pages/counter.tsx
+++ src/pages/counter.tsx
@@ -1,11 +1,12 @@
 import Counter from '@/component/Counter';
 import type { NextPage } from 'next';
+import styles from './button.module.css';
 
 
 const CounterPage: NextPage = () => {
     return (
         <div>
-            <h1>Welcome to the Counter Page</h1>
+            <h1 className={styles.error}>Welcome to the Counter Page</h1>
             <p>This is a simple example of integrating Redux with a counter component in Next.js.</p>
             <Counter />
         </div>
